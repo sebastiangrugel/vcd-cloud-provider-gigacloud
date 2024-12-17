30f675f04ed0ed2b6d47d4fd427cb4d3377a6d44
Sebastian Grugel <Sebastian.Grugel@ibm.com>
2024-12-17 15:30:06 +0100

    first commit

---
+++ training-class/resources.tf
@@ -0,0 +1,15 @@
+#data "vcd_catalog" "catalog" {
+#  name = "Sebastian Images"
+#}
+
+data "vcd_catalog" "catalog" {
+  name = "GigaCloud Images"
+}
+
+
+
+
+#data "vcd_catalog_vapp_template" "photon-hw11" {
+#  catalog_id = data.vcd_catalog.catalog
+#  name       = "CentOS_7"
+#}
